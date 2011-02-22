77ed8d3c8bd25ae94b37ecf149b33a15a6c7b74a
Drakia <Contact@TheDgtl.net>
2011-02-22 03:28:04 -0800

    Added iConomy support Added a function to handle messaging the player, now only the "[Warpz0r]" text is colored

--- src/net/TheDgtl/Warpz0r/Warpz0r.java
+++ src/net/TheDgtl/Warpz0r/Warpz0r.java
@@ -14,12 +14,12 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.PluginDescriptionFile;
-import org.bukkit.plugin.PluginLoader;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.PluginManager;
+import org.bukkit.util.config.Configuration;
 
 import com.nijikokun.bukkit.Permissions.Permissions;
+import com.nijikokun.bukkit.iConomy.Misc;
 import com.nijikokun.bukkit.iConomy.iConomy;
 
 /**
@@ -32,20 +32,21 @@ public class Warpz0r extends JavaPlugin {
     public Permissions Permissions = null;
     public static Logger log;
     public static Server server;
+    private Configuration config;
     private HashMap<String, World> worldList = new HashMap<String, World>();
     private String warpFile;
     private String homeFile;
     private World defWorld;
-
-    // Constructor for old version
-    /*
-    public Warpz0r(PluginLoader pluginLoader, Server instance, PluginDescriptionFile desc, File folder, File plugin, ClassLoader cLoader) {
-        super(pluginLoader, instance, desc, folder, plugin, cLoader);
-    }*/
+    
+    private boolean useiConomy;
+    private int warpCost;
+    private int homeCost;
+    private int setHomeCost;
     
     public void onEnable() {
     	log = Logger.getLogger("Minecraft");
     	PluginManager pm = getServer().getPluginManager();
+    	config = getConfiguration();
     	log.info( getDescription().getName() + " version " + getDescription().getVersion() + " is enabled" );
     	
         // Create data folder if it doesn't exist.
@@ -53,6 +54,7 @@ public class Warpz0r extends JavaPlugin {
         warpFile = getDataFolder().getPath() + File.separator + "warps.db";
         homeFile = getDataFolder().getPath() + File.separator + "homes.db";
         server = getServer();
+        loadConfig();
     	
     	// Load a list of worlds for warping between worlds.
 		for(World w : getServer().getWorlds()) {
@@ -64,6 +66,16 @@ public class Warpz0r extends JavaPlugin {
     	Plugin perm = pm.getPlugin("Permissions");
 	    if(perm != null) Permissions = (Permissions)perm;
 	    else log.info("[" + getDescription().getName() + "] Permission system not enabled.");
+	    
+	    // Setup iConomy
+	    if (useiConomy) {
+	    	Plugin icon = pm.getPlugin("iConomy");
+	    	if (icon == null) {
+	    		useiConomy = false;
+	    	} else {
+	    		Warpz0r.log.info("[Warpz0r] Using iConomy. Warp Cost: " + warpCost + " Home Cost: " + homeCost);
+	    	}
+	    }
 	    
 		// Check if a previous warps.txt exists, import if it does.
 		File oldFile = new File(warpFile.substring(0, warpFile.length() - 2) + "txt");
@@ -92,6 +104,23 @@ public class Warpz0r extends JavaPlugin {
     	log.info( getDescription().getName() + " version " + getDescription().getVersion() + " is disabled" );
     }
     
+    public void loadConfig() {
+    	config.load();
+    	useiConomy = config.getBoolean("useiconomy", false);
+    	warpCost = config.getInt("warpcost", 5);
+    	homeCost = config.getInt("homecost", 5);
+    	setHomeCost = config.getInt("sethomecost", 0);
+    	saveConfig();
+    }
+    
+    public void saveConfig() {
+    	config.setProperty("useiconomy", useiConomy);
+    	config.setProperty("warpcost", warpCost);
+    	config.setProperty("homecost", homeCost);
+    	config.setProperty("sethomecost", setHomeCost);
+    	config.save();
+    }
+    
     @Override
     public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
     	if (!(sender instanceof Player)) return false;
@@ -100,32 +129,41 @@ public class Warpz0r extends JavaPlugin {
     	String comName = command.getName().toLowerCase();
         if (comName.equals("warp")) {
         	if (!hasPerm(player, "warpz0r.warp", player.isOp())) {
-        		player.sendMessage(ChatColor.RED + "[Warpz0r] Permission Denied");
+        		sendMessage(player, "Permission Denied", true);
         		return true;
         	}
         	if (args.length != 1) {
         		return false;
         	}
-        	
+        	if (useiConomy && iConomy.db.get_balance(player.getName()) < warpCost) {
+        		sendMessage(player, "Insufficient funds to warp. Cost: " + Misc.formatCurrency(warpCost, iConomy.currency), true);
+        		return true;
+        	}
     		Location loc = Locations.getWarp(args[0]);
     		if (loc != null) {
     			if (!loc.getWorld().getName().equals(player.getWorld().getName()) && !hasPerm(player, "warpz0r.worldwarp", player.isOp())) {
-    				player.sendMessage(ChatColor.RED + "[Warpz0r] Not allowed to warp between worlds");
+    				sendMessage(player, "Not allowed to warp between worlds", true);
     				return true;
+    			}
+    			// Subtract iConomy
+    			if (useiConomy && warpCost > 0) {
+    				int bal = iConomy.db.get_balance(player.getName());
+    				iConomy.db.set_balance(player.getName(), bal - warpCost);
+    				sendMessage(player, "Deducted " + Misc.formatCurrency(warpCost, iConomy.currency) + " for warping", false);
     			}
     			// Keep the current vertical looking direction
     			loc.setPitch(player.getLocation().getPitch());
     			player.teleportTo(loc);
-    			player.sendMessage(ChatColor.GREEN + "[Warpz0r] Teleported to " + args[0]);
+    			sendMessage(player, "Teleported to " + args[0], false);
     			log.info("[Warpz0r] " + player.getName() + " teleported to " + args[0]);
     		} else {
-    			player.sendMessage(ChatColor.RED + "[Warpz0r] Warp not found.");
+    			sendMessage(player, "Warp not found.", true);
     			log.info("[Warpz0r] " + player.getName() + " tried to teleport to " + args[0]);
     		}
     		return true;
         } else if (comName.equals("setwarp")) {
         	if (!hasPerm(player, "warpz0r.set", player.isOp())) {
-        		player.sendMessage(ChatColor.RED + "[Warpz0r] Permission Denied");
+        		sendMessage(player, "Permission Denied", true);
         		return true;
         	}
         	if (args.length != 1) {
@@ -135,12 +173,12 @@ public class Warpz0r extends JavaPlugin {
         	Location loc = player.getLocation();
         	Locations.addWarp(loc, args[0]);
         	Locations.saveList(warpFile, Locations.warps);
-        	player.sendMessage(ChatColor.GREEN + "[Warpz0r] Warp Set: " + args[0]);
+        	sendMessage(player, "Warp Set: " + args[0], false);
         	log.info("[Warpz0r] " + player.getName() + " set warp " + args[0]);
         	return true;
         } else if (comName.equals("removewarp")) {
         	if (!hasPerm(player, "warpz0r.remove", player.isOp())) {
-        		player.sendMessage(ChatColor.RED + "[Warpz0r] Permission Denied");
+        		sendMessage(player, "Permission Denied", true);
         		return true;
         	}
         	if (args.length != 1) {
@@ -149,18 +187,18 @@ public class Warpz0r extends JavaPlugin {
         	
         	Location loc = Locations.getWarp(args[0]);
         	if (loc == null) {
-        		player.sendMessage(ChatColor.RED + "[Warpz0r] Warp not found.");
+        		sendMessage(player, "Warp not found.", true);
         		log.info("[Warpz0r] " + player.getName() + " tried to remove warp " + args[0]);
         		return true;
         	}
         	Locations.removeWarp(args[0]);
         	Locations.saveList(warpFile, Locations.warps);
-        	player.sendMessage(ChatColor.GREEN + "[Warpz0r] Warp removed: " + args[0]);
+        	sendMessage(player, "Warp removed: " + args[0], false);
         	log.info("[Warpz0r] " + player.getName() + " removed warp " + args[0]);
         	return true;
         } else if (comName.equals("listwarps")) {
         	if (!hasPerm(player, "warpz0r.list", player.isOp())) {
-        		player.sendMessage(ChatColor.RED + "[Warpz0r] Permission Denied");
+        		sendMessage(player, "Permission Denied", true);
         		return true;
         	}
         	Set<String> warpList = Locations.getWarpList();
@@ -170,14 +208,14 @@ public class Warpz0r extends JavaPlugin {
         		sb.append(warp.next());
         		while (warp.hasNext())
         			sb.append(", ").append(warp.next());
-        		player.sendMessage(ChatColor.GREEN + "[Warpz0r] Warps: " + ChatColor.WHITE + sb.toString());
+        		sendMessage(player, "Warps: " + ChatColor.WHITE + sb.toString(), false);
         	} else {
-        		player.sendMessage(ChatColor.RED + "[Warpz0r] Warp list is empty");
+        		sendMessage(player, "Warp list is empty", true);
         	}
         	return true;
         } else if (comName.equals("warpto")) {
         	if (!hasPerm(player, "warpz0r.warpto", player.isOp())) {
-        		player.sendMessage(ChatColor.RED + "[Warpz0r] Permission Denied");
+        		sendMessage(player, "Permission Denied", true);
         		return true;
         	}
         	if (args.length != 2) {
@@ -185,57 +223,82 @@ public class Warpz0r extends JavaPlugin {
         	}
         	Player target = getServer().getPlayer(args[0]);
         	if (target == null) {
-        		player.sendMessage(ChatColor.RED + "[Warpz0r] Target player not found");
+        		sendMessage(player, "Target player not found", true);
         		return true;
         	}
         	Location loc = Locations.getWarp(args[1]);
         	if (loc == null) {
-        		player.sendMessage(ChatColor.RED + "[Warpz0r] Warp not found");
+        		sendMessage(player, "Warp not found", true);
         		return true;
         	}
         	target.teleportTo(loc);
-			player.sendMessage(ChatColor.GREEN + "[Warpz0r] Teleported " + target.getName() + " to " + args[1]);
-			target.sendMessage(ChatColor.GREEN + "[Warpz0r] " + player.getName() + " teleported you to " + args[1]);
+        	sendMessage(player, "Teleported " + target.getName() + " to " + args[1], false);
+        	sendMessage(target, player.getName() + " teleported you to " + args[1], false);
 			log.info("[Warpz0r] " + player.getName() + " teleported " + target.getName() + " to " + args[1]);
 			return true;
         } else if (comName.equals("sethome")) {
         	if (!hasPerm(player, "warpz0r.sethome", player.isOp())) {
-        		player.sendMessage(ChatColor.RED + "[Warpz0r] Permission Denied");
+        		sendMessage(player, "Permission Denied", false);
         		return true;
+        	}
+        	if (useiConomy && setHomeCost > 0) {
+        		if (iConomy.db.get_balance(player.getName()) < setHomeCost) {
+        			sendMessage(player, "Insufficient funds to set home. Cost: " + Misc.formatCurrency(setHomeCost, iConomy.currency), true);
+        			return true;
+        		} else {
+    				int bal = iConomy.db.get_balance(player.getName());
+    				iConomy.db.set_balance(player.getName(), bal - setHomeCost);
+    				sendMessage(player, "Deducted " + Misc.formatCurrency(setHomeCost, iConomy.currency) + " for setting home", false);
+        		}
         	}
         	Location loc = player.getLocation();
         	Locations.addHome(loc, player.getName());
         	Locations.saveList(homeFile, Locations.homes);
-        	player.sendMessage(ChatColor.GREEN + "[Warpz0r] Home Set");
+        	sendMessage(player, "Home Set", false);
         	log.info("[Warpz0r] " + player.getName() + " set home");
         	return true;
         } else if (comName.equals("home")) {
         	if (!hasPerm(player, "warpz0r.home", player.isOp())) {
-        		player.sendMessage(ChatColor.RED + "[Warpz0r] Permission Denied");
+        		sendMessage(player, "Permission Denied", true);
+        		return true;
+        	}
+        	if (useiConomy && iConomy.db.get_balance(player.getName()) < homeCost) {
+        		sendMessage(player, "Insufficient funds to warp home. Cost: " + Misc.formatCurrency(homeCost, iConomy.currency), true);
         		return true;
         	}
     		Location loc = Locations.getHome(player.getName());
     		if (loc != null) {
     			if (!loc.getWorld().getName().equals(player.getWorld().getName()) && !hasPerm(player, "warpz0r.worldhome", true)) {
-    				player.sendMessage(ChatColor.RED + "[Warpz0r] Not allowed to teleport home between worlds");
+    				sendMessage(player, "Not allowed to teleport home between worlds", true);
     				return true;
+    			}
+    			// Subtract iConomy
+    			if (useiConomy && homeCost > 0) {
+    				int bal = iConomy.db.get_balance(player.getName());
+    				iConomy.db.set_balance(player.getName(), bal - homeCost);
+    				sendMessage(player, "Deducted " + Misc.formatCurrency(homeCost, iConomy.currency) + " for teleporting home", false);
     			}
     			// Keep the current vertical looking direction
     			loc.setPitch(player.getLocation().getPitch());
     			player.teleportTo(loc);
-    			player.sendMessage(ChatColor.GREEN + "[Warpz0r] Teleported to home");
+    			sendMessage(player, "Teleported to home", false);
     			log.info("[Warpz0r] " + player.getName() + " teleported to home");
     		} else {
-    			player.sendMessage(ChatColor.RED + "[Warpz0r] Home not set");
+    			sendMessage(player, "Home not set", false);
     		}
     		return true;
         }
         return false;
 	}
     
+    private static void sendMessage(Player p, String message, boolean error) {
+    	ChatColor color = (error) ? ChatColor.RED : ChatColor.GREEN;
+    	p.sendMessage(color + "[Warpz0r] " + ChatColor.WHITE + message);
+    }
+    
     public Boolean hasPerm(Player player, String perm, Boolean def) {
     	if (Permissions != null)
-    		return Permissions.Security.has(player, perm);
+    		return com.nijikokun.bukkit.Permissions.Permissions.Security.has(player, perm);
     	return def;
     }
 }
